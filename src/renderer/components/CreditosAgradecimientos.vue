8dd6c7b3c8f9b84ac34f0328587227f24dfa77b6
Yubisel <yubiselv@gmail.com>
2018-10-30 11:22:20 -0400

    puesto isbn y editorial

--- src/renderer/components/CreditosAgradecimientos.vue
+++ src/renderer/components/CreditosAgradecimientos.vue
@@ -2,6 +2,20 @@
     <vue-scrollbar classes="my-scrollbar" ref="Scrollbar">
         <div class="inner-page glosario">
             <div v-html="searchAndHighlight(searchText, content)"></div>
+
+            <div class="logos-container">
+                <div class="left">
+                    <h4 class="text-red">ISSN</h4>
+                    <img src="~@/assets/images/issn.jpg" alt="editorial" style="margin: 0 auto;">
+                </div>
+                <div class="right">
+                    <h4 class="text-red">Ediciones Bayamo</h4>
+                    <img src="~@/assets/images/logo_editorial.jpg" alt="editorial" style="margin: 0 auto;">
+                </div>
+            </div>
+<br>
+
+            <div v-html="searchAndHighlight(searchText, content2)"></div>
         </div>
     </vue-scrollbar>
 </template>
@@ -21,9 +35,8 @@ export default{
                         <li><span>Programación:</span> Ing. Raúl Alejandro Rondón Milanés</li>
                         <li><span>Programación:</span> Ing. Yubisel Vega Alvarez</li>
                         <li><span>Redacción y corrección de textos:</span> MSc. Linnet Zurina Rodríguez Blanco</li>
-                    </ul>
-                    <p>ISBN: Solicitado</p>
-                    <h3>Fotos cortesía de:</h3>
+                    </ul>`,
+                    content2: `<h3>Fotos cortesía de:</h3>
                     <p>Mesa Bauzá, Ramón. Pintor. Centro de Desarrollo de las Artes Visuales de Granma. Foto No. 36. </p>
                     <p>Valerino Cámbar, Lino. Presidente de la Sociedad de Fotógrafos de Bayamo. Fotos No. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24 y 25. </p>
                     <p>Yero Masdeu, José Manuel. Investigador de la Casa de la Nacionalidad  Cubana. Bayamo, Granma. Fotos No. 16, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33, 34 y 35. </p>
@@ -38,6 +51,18 @@ export default{
     }
 }
 </script>
-<style lang="scss" scoped>
-
+<style lang="scss">
+    .logos-container{
+        margin-top: 20px;
+        .left,
+        .right{
+            display: inline-block;
+            width: 45%;
+            text-align: center;
+            img{
+                height: 80px;
+                width: auto;
+            }
+        }
+    }
 </style>
